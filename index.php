6796ecb28ee190cb490cb7880caf751b96302213
Artadina Septiani <88015690+artadina@users.noreply.github.com>
2021-09-07 20:07:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Belajar Laravel</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Class Laravel</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="tampil_siswa.php">Tampil Siswa</a></li>
+      <li><a href="tampil_kelas.php">Tampil Kelas</a></li>
+      <li><a href="tambah_siswa.php">Tambah Siswa</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>WELCOME TO LARAVEL CLASS XI RPL 5</h3>
+  <p>Today we gonna study about DATABASE.</p>
+</div>
+
+</body>
+</html>
