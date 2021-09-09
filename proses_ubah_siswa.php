dce57bf7554111a42a15cf3bbb20453308704a03
Artadina Septiani <88015690+artadina@users.noreply.github.com>
2021-09-09 17:58:49 +0700

    Add files via upload

--- proses_ubah_siswa.php
+++ proses_ubah_siswa.php
@@ -10,27 +10,27 @@ if($_POST){
     $id_kelas=$_POST['id_kelas'];
     if(empty($nama_siswa)){
         echo "<script>alert('nama siswa tidak boleh kosong');location.href='tambah_siswa.php';</script>";
+
     } elseif(empty($username)){
         echo "<script>alert('username tidak boleh kosong');location.href='tambah_siswa.php';</script>";
     } else {
         include "koneksi.php";
         if(empty($password)){
-            $update=mysqli_query($conn,"update siswa set nama_siswa='".$nama_siswa."',tanggal_lahir='".$tanggal_lahir."',gender='".$gender."', alamat='".$alamat."', username='".$username."', id_kelas='".$id_kelas."' where id_siswa = '".$id_siswa."' ") 
-            or die(mysqli_error($conn));
+            $update=mysqli_query($conn,"update siswa set nama_siswa='".$nama_siswa."',tanggal_lahir='".$tanggal_lahir."', gender='".$gender."', alamat='".$alamat."', username='".$username."', id_kelas='".$id_kelas."' where id_siswa = '".$id_siswa."' ") or die(mysqli_error($conn));
             if($update){
                 echo "<script>alert('Sukses update siswa');location.href='tampil_siswa.php';</script>";
             } else {
                 echo "<script>alert('Gagal update siswa');location.href='ubah_siswa.php?id_siswa=".$id_siswa."';</script>";
             }
         } else {
-            $update=mysqli_query($conn,"update siswa set nama_siswa='".$nama_siswa."', tanggal_lahir='".$tanggal_lahir."', gender='".$gender."', alamat='".$alamat."', username='".$username."', password='".md5($password)."', id_kelas='".$id_kelas."' where id_siswa = '".$id_siswa."'")
-            or die(mysqli_error($conn));
+            $update=mysqli_query($conn,"update siswa set nama_siswa='".$nama_siswa."',tanggal_lahir='".$tanggal_lahir."', gender='".$gender."', alamat='".$alamat."', username='".$username."', password='".md5($password)."', id_kelas='".$id_kelas."' where id_siswa = '".$id_siswa."'") or die(mysqli_error($conn));
             if($update){
-                echo "<script>alert('Sukses update siswa');location.href='tampil_siswa.php'</script>";
+                echo "<script>alert('Sukses update siswa');location.href='tampil_siswa.php';</script>";
             } else {
                 echo "<script>alert('Gagal update siswa');location.href='ubah_siswa.php?id_siswa=".$id_siswa."';</script>";
             }
         }
-    }
+        
+    } 
 }
 ?>
